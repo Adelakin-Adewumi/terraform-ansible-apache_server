f62c7c9cf8e917888314dedb8d652d34230da83c
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2023-02-05 05:10:38 -0800

    Update route53.tf

--- mini-project/route53.tf
+++ mini-project/route53.tf
@@ -6,7 +6,7 @@ data "aws_route53_zone" "hosted_zone" {
 resource "aws_route53_record" "site_domain" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = var.record_name
-  type    = "PTR"
+  type    = "NS"
 
   alias {
     name                   = aws_elb.bar-foo.dns_name
